55249fe0a2da5d83c54ad9fa91d7e15bc6664fdd
Cesar Husillos <cesar_husillos@tutanota.com>
2021-08-13 13:47:36 +0200

    Primera version operativa de astmon.py

--- astmon.py
+++ astmon.py
@@ -4,12 +4,15 @@
 
 import argparse
 import os
+import copy
 
-# import seaborn as sns
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates as mdates
+
+import sqlite3
 
 def classify(data):
     """
@@ -47,6 +50,50 @@ def classify(data):
         cat.append(value)
     return np.array(cat)
 
+def time_intervals(period, years, months, days):
+    """Get time interval for data to plot."""
+    dts = []
+    if period:
+        dts = [copy.deepcopy(period)]
+    else:
+        dts_ini = [f"{y}-{m}-{d} 00:00:00" for d in days for m in months for y in years]
+        dts_final = [dt.replace('00:00:00', '23:59:59') for dt in dts_ini]
+
+        dts = zip(dts_ini, dts_final)
+
+    return dts
+
+def get_data(db_file, period, years, months, days, positions, filters):
+    """"""
+
+    dts = time_intervals(period, years, months, days)
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+
+    keywords = ['datetime_obs', 'is_moon', 'photo_night', 'sky_bright', 'position', 'filter_name']
+
+    sql = f"SELECT datetime(datetime_obs, 'unixepoch') as datetime_obs, {','.join(keywords[1:])} FROM measurement WHERE "
+    wheres = []
+    if positions:
+        wheres.append(f"position in ({', '.join(positions)})")
+    if filters:
+        wheres.append(f"filter_name in ('{', '.join(filters)}')")
+    if dts:
+        intervals = [f"datetime_obs BETWEEN strftime('%s', '{dti}') and strftime('%s', '{dtf}')" for dti, dtf in dts]
+        
+        wheres.append(f"({' OR '.join(intervals)})")
+    
+    sql += ' AND '.join(wheres)
+
+    print(f"sql = {sql}")
+    c.execute(sql)
+
+    res = c.fetchall()
+
+    df = pd.DataFrame(res, columns=keywords)
+
+    return df
+
 def plot_data(data, out_plot, title, field_group='category_night'):
     """
     Plot information contained in input argument 'data'.
@@ -54,7 +101,7 @@ def plot_data(data, out_plot, title, field_group='category_night'):
     A bit longer description.
 
     Args:
-        data (dataframe): Input pandas dataframe. Fields are ['date', 'time', 'is_moon', 'photo_night', 'sky_bright', 'category_night'].
+        data (pandas.dataframe): Fields are ['date', 'time', 'is_moon', 'photo_night', 'sky_bright', 'category_night'].
         out_plot (str): Path for output plot.
         title (str): Title for output plot.
         field_group (str): Field used for grouping.
@@ -63,7 +110,7 @@ def plot_data(data, out_plot, title, field_group='category_night'):
         int: 0 - everything was fine.
 
     Raises:
-        Exception: Type of exception depends on failed line of code.
+        Exception: Exception type depends on failed line of code.
 
     """
     # Grouping dataframe
@@ -96,28 +143,90 @@ def plot_data(data, out_plot, title, field_group='category_night'):
 def main():
     parser = argparse.ArgumentParser(prog='astmon.py',
                                      conflict_handler='resolve',
-                                     description='''This program reads, categorizes and plots data from input file. ''',
-                                     epilog='''''')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-    parser.add_argument("input_file", help="Input data file")
-    parser.add_argument("--output_dir",
+                                     description='''It plots data from database SQLite file. 
+                                     
+                                     - If period is given, 'years', 'months' and 'days' parameters are ignored.
+                                     - If 'filters' are empty, every filters are plotted.
+                                     - If no 'positions' are given, all values are plotted.
+                                     
+                                     Use example:
+                                     python astmon.py ''',
+                                     epilog='''If period is set, then 
+                                        "years", "months", "days" 
+                                     parameters are ignored.''')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    parser.add_argument("db_file", help="SQLite file database path")
+    parser.add_argument("--output_plot_dir",
                         action="store",
-                        dest="output_dir",
+                        dest="output_plot_dir",
                         default="./",
                         help="Output plot directory [default: %(default)s]")
+    parser.add_argument("--period",
+                        action="store",
+                        nargs="+", 
+                        default=[],
+                        dest="period",
+                        help="Datetime period (init, final) with 'init' and 'final' format: YYYY-MM-DD hh:mm:ss  [default: %(default)s]")
+    parser.add_argument("--years",
+                        nargs="+", 
+                        default=[],
+                        action="store",
+                        dest="years",
+                        help="Input list of years for plotting [default: %(default)s]")
+    parser.add_argument("--months",
+                        nargs="+", 
+                        default=[],
+                        action="store",
+                        dest="months",
+                        help="Numerical list of months to plot (allowed values: [1, 12]) (1-January,..., 12-december) [default: %(default)s]")
+    parser.add_argument("--days",
+                        nargs="+", 
+                        default=[],
+                        action="store",
+                        dest="days",
+                        help="Numerical list of days to plot (allowed values: [1, 31]) [default: %(default)s]")
+    parser.add_argument("--positions",
+                        nargs="+", 
+                        default=[],
+                        action="store",
+                        dest="positions",
+                        help="Numerical list of positions for plotting (allowed values: [1, 10]) [default: %(default)s]")
+    parser.add_argument("--filters",
+                        nargs="+", 
+                        default=[],
+                        action="store",
+                        dest="filters",
+                        help="List of filter names to plot (allowed values: ('B', 'V', 'R', 'I') [default: %(default)s]")
     parser.add_argument('-v', '--verbose', action='count', default=0,
                         help="Show running and progress information [default: %(default)s].")
     args = parser.parse_args()
 
-    # Fixed field with format file
-    data = pd.read_fwf(args.input_file, header=None,
-                   names=['date', 'time', 'is_moon', 'photo_night', 'sky_bright'],
-                   index_col=False)
-    # combining two columns
-    dt = data['date'] + ' ' + data['time']
+    # args = vars(parser.parse_args())
+    print(args)
+
+    # return 2
+
+    data = get_data(args.db_file, args.period, args.years, args.months, \
+        args.days, args.positions, args.filters)
+
+    if len(data.index) == 0:
+        print("WARNING: No data registered for these parameters")
+        return 1
+
+    class_data = classify(data)
+
+    data['category_night'] = class_data
+
+    print(data.info())
+    print(data.head())
+    # return 1
+
     # Changing str to datetime type
-    data['datetime'] = pd.to_datetime(dt, dayfirst=True)
+    data['datetime'] = pd.to_datetime(data['datetime_obs'], dayfirst=True)
     print(data)
+
+    print(f"Included filters = {np.unique(data['filter_name'].values)}")
+    print(f"Included positions = {np.unique(data['position'].values)}")
 
     # Setting datetime as index for dataframe (useful for time series works)
     data.set_index("datetime", inplace = True)
@@ -126,8 +235,19 @@ def main():
     data['category_night'] = classify(data)
 
     # Plotting 
-    input_name = os.path.splitext(os.path.split(args.input_file)[1])[0]
-    out_plot = os.path.join(args.output_dir, input_name + '.jpg')
+    # input_name = os.path.splitext(os.path.split(args.input_file)[1])[0]
+    input_name = 'sky_measures'
+    if args.positions:
+        input_name += f"_positions-{','.join(args.filters)}"
+    else:
+        input_name += "_positions-all"
+
+    if args.filters:
+        input_name += f"_filters-{','.join(args.filters)}"
+    else:
+        input_name += "_filters-all"
+
+    out_plot = os.path.join(args.output_plot_dir, input_name + '.jpg')
     title = input_name
     sns.set_style("darkgrid")
     return plot_data(data, out_plot, title)
